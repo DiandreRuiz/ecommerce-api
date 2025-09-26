b4af780348329ab61a7c0d7ffbcb240bd1735a78
DiandreRuiz <Diandreruiz77@gmail.com>
2025-09-26 17:00:12 -0400

    Commented out error line to ensure stability

--- app.py
+++ app.py
@@ -14,9 +14,11 @@ app = Flask(__name__)
 # Configure MySQL data in app w/ db connection URI
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_CONN_STR
 
+
 # Creation of Base Model we will use to store to metadata
 class Base(DeclarativeBase):
     pass
+
 
 # Initialize SQLAlchemy and marshmallow
 db = SQLAlchemy(model_class=Base)
@@ -24,9 +26,4 @@ db.init_app(app)
 marsh = Marshmallow(app)
 
 # Models & Junction Tables
-order_product = 
-
-
-
-
-
+# order_product =
